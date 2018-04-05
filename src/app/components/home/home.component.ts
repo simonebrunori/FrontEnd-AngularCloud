c5554497443526d94a2d5a9865d0f886e084b6e7
Simone Brunori <brunorisimone@gmail.com>
2018-04-05 19:30:11 +0100

    Sistemazione routes e implementazione logout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router:Router) { }
+
+  //Logout function
+  logout(){
+    this.authService.logout();  //call authService logout function 
+    this.router.navigate(['/login']); 
+  }
 
   ngOnInit() {
+    //Check if user is logged on component load 
+    if(!this.authService.loggedIn()){
+      this.router.navigate(['/login']); //if user is not logged, go to login
+    }
   }
 
 }
